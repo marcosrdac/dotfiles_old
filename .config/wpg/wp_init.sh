9c49730bdb039558a05e68b553ab8adc1f73e239
marcosrdac <marcosrdac@gmail.com>
2019-10-13 13:36:27 -0300

    brand new blinking pomodoro script

--- .config/wpg/wp_init.sh
+++ .config/wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs '/home/marcosrdac/pictures/wallpapers/favorites/arara_azul_brazil_total_gc.jpg' '/home/marcosrdac/pictures/wallpapers/favorites/arara_azul_brazil_total_gc.jpg'
+wpg -rs 'astronaut_with_coloured_jellyfish_completion.jpg' 'astronaut_with_coloured_jellyfish_completion.jpg'
--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#5c1c5a
-newglyph=#5f550e
+oldglyph=#496749
+newglyph=#773436
 
 #	Front
 #	default color: 36d7b7
-oldfront=#a934a4
-newfront=#b29f1a
+oldfront=#7db07d
+newfront=#cb5b5d
 
 #	Back
 #	default color: 1ba39c
-oldback=#742371
-newback=#7a6d12
+oldback=#567956
+newback=#8c3e40
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
