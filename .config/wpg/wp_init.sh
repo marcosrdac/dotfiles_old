5e787147a34daa65640e8b1732365d7e6cf84e9d
marcosrdac <marcosrdac@gmail.com>
2020-03-21 15:50:11 -0300

    some changes to lfrc, adding trash

--- .config/wpg/wp_init.sh
+++ .config/wpg/wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs 'nord.png' 'nord.png'
+wpg -rs '/home/marcosrdac/pictures/wallpapers/favorites/bridge_pink_blue_peaceful_wakening_gc.jpg' '/home/marcosrdac/pictures/wallpapers/favorites/bridge_pink_blue_peaceful_wakening_gc.jpg'
--- .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
+++ .local/share/icons/flattrcolor/scripts/replace_folder_file.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-oldglyph=#4f3c35
-newglyph=#4a5640
+oldglyph=#282f3c
+newglyph=#6f4753
 
 #	Front
 #	default color: 36d7b7
-oldfront=#8f6c5f
-newfront=#829770
+oldfront=#4c5973
+newfront=#be798d
 
 #	Back
 #	default color: 1ba39c
-oldback=#624a41
-newback=#59684d
+oldback=#343d4f
+newback=#825361
 
 sed -i "s/#524954/$oldglyph/g" $1
 sed -i "s/#9b8aa0/$oldfront/g" $1
